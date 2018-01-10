9cf854c3baac86d7f8fcc9742ee42565d37301ba
platonovAlexey <kalyawe4ka@gmail.com>
2018-01-10 12:10:01 +0300

    add animation, adaptive

--- src/js/modules/changeBackgroundCase.js
+++ src/js/modules/changeBackgroundCase.js
@@ -1,18 +1,27 @@
 $(window).scroll(function(){
-    if($(window).scrollTop() > '50'){
+    if($(window).scrollTop() == '0'){
         $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.0)');
     }
-    if ($(window).scrollTop() > '150') {
+    if($(window).scrollTop() > '30'){
+        $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.1)');
+    }
+    if ($(window).scrollTop() > '50') {
         $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.2)');
     } 
-    if($(window).scrollTop() > '200'){
+    if($(window).scrollTop() > '100'){
+        $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.3)');
+    } 
+    if($(window).scrollTop() > '150'){
         $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.4)');
     } 
+    if($(window).scrollTop() > '200'){
+        $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.5)');
+    }
     if($(window).scrollTop() > '250'){
         $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.6)');
-    } 
+    }
     if($(window).scrollTop() > '300'){
-        $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.8)');
+        $('.mvideo-header-mask').css('background', 'rgba(95,31,155, 0.7)');
     }
     
 });
